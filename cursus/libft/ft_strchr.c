e33087f6c65d635b04a51c4ccbace95f0a2adbae
rikrdo <echeverriabarrena@gmail.com>
2022-04-27 08:54:04 +0200

    Add new functions

--- cursus/libft/ft_strchr.c
+++ cursus/libft/ft_strchr.c
@@ -1,30 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rechever <rechever@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/24 17:09:13 by rechever          #+#    #+#             */
-/*   Updated: 2022/04/27 08:33:01 by rechever         ###   ########.fr       */
+/*   Created: 2022/04/27 07:50:08 by rechever          #+#    #+#             */
+/*   Updated: 2022/04/27 08:45:23 by rechever         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
 
-#ifndef LIBFT_H
-# define LIBFT_H
-
-# include <stdio.h>
-# include <unistd.h>
-# include <stdlib.h>
-
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
-int ft_toupper(int c);
-int ft_tolower(int c);
-size_t	ft_strlen(const	char *t);
-char *ft_strchr(const char *s, int c);
-
-#endif
+char *ft_strchr(const char *s, int c)
+{
+	int	i;
+	if (!s)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			return ((char *)(s+i));
+		i++;
+	}
+	return (NULL);
+}
